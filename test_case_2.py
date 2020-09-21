1c86fcb0f5cf5da1a20e330cdace1a34f3cacf90
JuliaFedyaeva <julia.fedyaeva@mail.ru>
2020-09-21 13:25:48 +0300

    Правит 2 тест

--- test_case_2.py
+++ test_case_2.py
@@ -3,11 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import locators as _locators
+import test_data
+import utils
 
 
 def search_item():
     # Data
-    book_name = 'Google Hacking'
     book_locator = 'a[title="Google Hacking"]'
 
     try:
@@ -16,17 +17,16 @@ def search_item():
         browser.get(_locators.main_page_link)
 
         # Steps
-        browser.find_element_by_css_selector(_locators.input_search).send_keys(book_name)
-        browser.find_element_by_css_selector(_locators.input_search_button).click()
+        utils.authorizaite(browser, test_data.book_name)
 
         WebDriverWait(browser, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, book_locator))
         )
 
         # Assert
-        book_name_text = browser.find_element_by_css_selector(book_locator).text
-        assert book_name in book_name_text, \
-            "Search item '%s' should contain text '%s'" % (book_name_text, book_name)
+        book_name_text = utils.find(browser, book_locator).text
+        assert test_data.book_name in book_name_text, \
+            "Search item '%s' should contain text '%s'" % (book_name_text, test_data.book_name)
 
     finally:
         browser.quit()
