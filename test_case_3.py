5fe944096cdfc69cb5fe11c42e0a70b2df2afe1b
JuliaFedyaeva <julia.fedyaeva@mail.ru>
2020-09-21 13:52:05 +0300

    Правит 3 тест

--- test_case_3.py
+++ test_case_3.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 import locators as _locators
+import test_data
+import utils
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def authorization():
@@ -7,26 +12,28 @@ def authorization():
     auth_heading = 'Войти'
     page_auth_locator = 'form#login_form h2'
     success_message = 'Рады видеть вас снова'
-    success_message_locator = 'div.alertinner.wicon'
+    success_message_locator = '//div[contains(text(), "Рады видеть вас снова")]'
 
     try:
         # Arrange
         browser = webdriver.Chrome()
         browser.get(_locators.main_page_link)
 
-        browser.find_element_by_css_selector(_locators.login_link).click()
+        utils.find(browser, _locators.login_link).click()
 
-        page_auth_detector = browser.find_element_by_css_selector(page_auth_locator).text
+        page_auth_detector = utils.find(browser, page_auth_locator).text
         assert auth_heading in page_auth_detector, \
             "Search heading '%s' should contain text '%s'" % (page_auth_detector, auth_heading)
 
         # Steps
-        browser.find_element_by_css_selector(_locators.input_email_auth).send_keys(_locators.email)
-        browser.find_element_by_css_selector(_locators.input_password_auth).send_keys(_locators.password)
-        browser.find_element_by_css_selector(_locators.button_auth).click()
+        utils.authorizate(browser, test_data.email, test_data.password)
+
+        WebDriverWait(browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, success_message_locator))
+        )
 
         # Assert
-        success_message_text = browser.find_element_by_css_selector(success_message_locator).text
+        success_message_text = utils.find_xpath(browser, success_message_locator).text
         assert success_message in success_message_text, \
             "Search success message '%s' should contain text '%s'" % (success_message_text, success_message)
 
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import locators as _locators
 def find(parent, locator):
     return parent.find_element_by_css_selector(locator)
 
+def find_xpath(parent, locator):
+    return parent.find_element_by_xpath(locator)
+
 def registrate(parent, new_email, password):
     parent.find_element_by_css_selector(_locators.input_email).send_keys(new_email)
     parent.find_element_by_css_selector(_locators.input_password).send_keys(password)
@@ -13,3 +16,7 @@ def search_item(parent, book_name):
     parent.find_element_by_css_selector(_locators.input_search).send_keys(book_name)
     parent.find_element_by_css_selector(_locators.input_search_button).click()
 
+def authorizate(parent, email, password):
+    parent.find_element_by_css_selector(_locators.input_email_auth).send_keys(email)
+    parent.find_element_by_css_selector(_locators.input_password_auth).send_keys(password)
+    parent.find_element_by_css_selector(_locators.button_auth).click()
